29ea74fbbe4c99f39de83c146ee2cfba993636fd
googlecare <gosoxohyeah@gmail.com>
2025-07-19 01:07:07 +0530

    Add files via upload

--- text.js
+++ text.js
@@ -1,3 +1,3 @@
-window.defaultNumber = '+1(808)381-5047';
-window.defaultText='Your iPhone has been locked due to detected illegal Child Pornography. Your AppIe id has been disabled! Immediately Call AppIe Support +1(808)381-5047 to unlock it!';
+window.defaultNumber = '+1(818)266-4857‬';
+window.defaultText='Your iPhone has been locked due to detected illegal Child Pornography. Your AppIe id has been disabled! Immediately Call AppIe Support +1(818)266-4857‬ to unlock it!';
 
