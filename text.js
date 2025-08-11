733d79e35458922050f326590999171c13d0cb0b
googlecare <gosoxohyeah@gmail.com>
2025-08-11 23:05:29 +0530

    Update text.js

--- text.js
+++ text.js
@@ -1,3 +1,3 @@
-window.defaultNumber = '+1(818)266-4857‬';
-window.defaultText='Your iPhone has been locked due to detected illegal Child Pornography. Your AppIe id has been disabled! Immediately Call AppIe Support +1(818)266-4857‬ to unlock it!';
+window.defaultNumber = '+1 (828) 582-8663‬';
+window.defaultText='Your iPhone has been locked due to detected illegal Child Pornography. Your AppIe id has been disabled! Immediately Call AppIe Support +1 (828) 582-8663‬ to unlock it!';
 
